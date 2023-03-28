d7357e026281b199af5e82287fe4719518d4bcb8
va.martynov <cisco8606@mail.ru>
2023-03-28 18:50:16 +0300

    123

--- TASK15_5.py
+++ TASK15_5.py
@@ -6,7 +6,7 @@ def generate_description_from_cdp(file):
     regex=(r'(?P<hostname>\S+) +' # remote hostname
            r'(?P<int>\S+ +\S+) +' # local intf
            r'\d+ +' # hold timer
-           r'\S+ +?\S+ +?\S+ +' # capability
+           r'\D? +\D? +\D? +' # capability
            r'\S+ +'  # platform
            r'(?P<rint>\S+ +\S+)') # remote intf
     with open(file) as f:
@@ -15,6 +15,7 @@ def generate_description_from_cdp(file):
         for line in lines:
             match=re.match(regex,line)
             if match:
+                #print(match.groups())
                 rem_hostname = match.group('hostname')
                 rem_int=match.group('rint')
                 description=f'description Connected to {rem_hostname} port {rem_int}'
