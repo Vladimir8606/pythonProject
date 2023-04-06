f224187380c84061868ac0a6247bc798f35d902f
va.martynov <cisco8606@mail.ru>
2023-04-06 17:28:14 +0300

    123

--- test2.py
+++ test2.py
@@ -1,13 +1,3 @@
-
-list_of_files='C:\\python\\sh_cdp_n_sw1.txt'
-save_to_filename='C:\\python\\cdp.yaml'
-
-
-def generate_topology_from_cdp(list_of_files, save_to_filename):
-    list1=str(save_to_filename).split('\\')
-    print(list1[2])
-
-
-
-
-generate_topology_from_cdp(list_of_files, save_to_filename)
+list1=[1,2,3]
+list2=list1[1],list1[0],list1[0]
+print(list(list2))
