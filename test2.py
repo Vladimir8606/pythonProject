b20009b94246a4163badd2dbccf53bcbcc4fba09
Vladimir8606 <cisco8606@mail.ru>
2023-05-07 14:00:00 +0200

    123

--- test2.py
+++ test2.py
@@ -1,5 +1,16 @@
-import getpass
+class Switch:
+    def info(self):
+        print(f"Hostname: {self.hostname}\nModel: {self.model}")
 
-password = getpass.getpass()
+    def generate_interfaces(self, intf_type, number_of_intf):
+        interfaces = [f"{intf_type}{number}" for number in range(1, number_of_intf + 1)]
+        self.interfaces=interfaces
 
-print(password)
+
+sw1 = Switch()
+sw1.hostname = 'sw1'
+sw1.model = 'Cisco 3850'
+sw1.generate_interfaces('Fa0/', 10)
+print(sw1.interfaces)
+sw1.info()
+print('123')
